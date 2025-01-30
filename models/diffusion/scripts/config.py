a08dea60dca56e43cecf6ae9f07f8c556ccaeca4
jjj5306 <jjj5306@naver.com>
2025-01-30 21:59:26 +0900

    fix : updata model save path

--- models/diffusion/scripts/config.py
+++ models/diffusion/scripts/config.py
@@ -11,9 +11,9 @@ class TrainingConfig:
     #작은 배치크기로도 충분하면 1을 사용할수도 있지만 모델이 큰 배치를 필요로 하고 GPU 메모리가 부족한경우 2, 4 를 사용하기도 함, 가짜로 배치사이즈를 늘려주는 효과를 제공
     
     num_epochs = 100 #에포크 수
-    num_train_timestpes = 1000 #타임스텝의 수
-    save_image_epochs = 10
-    save_model_epochs = 30
+    num_train_timesteps = 1000 #타임스텝의 수
+    save_image_epochs = 5
+    save_model_epochs = 5
     
     learning_rate = 1e-4
     lr_warmup_steps = 500
@@ -21,7 +21,7 @@ class TrainingConfig:
     mixed_precision = "fp16"  # `no`는 float32, 자동 혼합 정밀도를 위한 `fp16`
     
     output_dir = "/home/minelab/desktop/ANN/Taehwa2/himeow-eye/models/diffusion/output/"  # 로컬 및 HF Hub에 저장되는 모델명
-    model_save_dir = "/home/minelab/desktop/ANN/Taehwa2/himeow-eye/models/diffusion/output/"
+    model_save_dir = "/home/minelab/desktop/ANN/Taehwa2/himeow-eye/models/diffusion/output/model"
     
     seed = 0 # evaluate.py에서 사용
     
--- models/diffusion/scripts/train.py
+++ models/diffusion/scripts/train.py
@@ -18,61 +18,83 @@ def train_loop(config, model, noise_scheduler, optimizer, train_dataloader, lr_s
         mixed_precision=config.mixed_precision,
         gradient_accumulation_steps=config.gradient_accumulation_steps, 
         log_with="tensorboard",
-        project_dir=project_dir,
+        project_dir="project_dir",
     )
     
-    # prepare 호출전에 train_dataloader의 길이를
-    
-    # Prepare everything
-    # There is no specific order to remember, you just need to unpack the 
-    # objects in the same order you gave them to the prepare method.
+    #분산학습환경에 맞게 acceleraotr을 준비 - 여러 GPU에서 병렬 처리 가능
     model, optimizer, train_dataloader, lr_scheduler = accelerator.prepare(
         model, optimizer, train_dataloader, lr_scheduler
     )
     
+    #전체학습 loop에서 몇 번째 step인치 추적하기 위해 global_step을 설정
+    #모니터링 할때 global_step이 기준이됨
+    #한 epoches에서 뿐만 아니라 전체 스텝
     global_step = 0
 
-    # Now you train the model
     for epoch in range(config.num_epochs):
         progress_bar = tqdm(total=len(train_dataloader), disable=not accelerator.is_local_main_process)
         progress_bar.set_description(f"Epoch {epoch}")
 
+        #step은 현재 배치가 몇번째인지를 나타내는 정수, 매 반복마다 1씩 증가
+        #batch는 trian_dataloader에서 받아오는 데이터 묶음
+        #batch를 통해 받아온 텐서에 무작위 노이지를 추가한뒤, 모델이 예측해야하는 noise_residual과 비교하여 손실(MSE)을 구함
+        
+        #########배치 처리###########
+        #step : 학습루프에서 현재 배치의 index(몇번째 배치인지)
+        #batch : 현재 배치의 데이터
+        #train_dataloader에서 배치단위로 처리하는 loop
+
+
         for step, batch in enumerate(train_dataloader):
-            # print(type(batch), len(batch), batch)
-            # break #batch 의 타입을 확인하고 아래  [ ]안에 들어갈 변수를 위한 코드드
+            
+            #현재 배치에서 원본 이미지 데이터를 가져옴            
             clean_images = batch[0]
-            # Sample noise to add to the images
+            
+            # 랜덤 노이즈 생성
+            # 원본 이미지와 동일한 크기의 랜덤 노이즈 텐서를 생성
+            #.to 부분 : 생성된 노이즈 텐서를 이미지 텐서와 동일한 디바이스(GPU 또는 CPU)로 이동                        
             noise = torch.randn(clean_images.shape).to(clean_images.device)
             
+            #텐서에서 첫 번째 차원(배치 크기)을 가져옴
+            #배치 크기 : 16
             bs = clean_images.shape[0]
 
-            # Sample a random timestep for each image
+            # 이미지에 대해 무작위로 타임스텝을 샘플링
             timesteps = torch.randint(
                 0, noise_scheduler.config.num_train_timesteps, (bs,), device=clean_images.device
             ).long()
 
-            # Add noise to the clean images according to the noise magnitude at each timestep
-            # (this is the forward diffusion process)
+            #forward processing - 이미지에 노이즈를 추가가
             noisy_images = noise_scheduler.add_noise(clean_images, noise, timesteps)
             
             with accelerator.accumulate(model):
-                # Predict the noise residual
+                # 노이즈 잔여량 예측
+                # 1.모델이 노이즈가 추가된 이미지(noisy_image)를 입력으로 받고 예측
+                # 2.평균제곱오차를 총해 실제노이즈와 모델이 예측한 노이즈를 계산
+                # 3.loss값을 기반으로 gradient 계산및 역전파 수행
                 noise_pred = model(noisy_images, timesteps, return_dict=False)[0]
                 loss = F.mse_loss(noise_pred, noise)
                 accelerator.backward(loss)
 
-                accelerator.clip_grad_norm_(model.parameters(), 1.0)
-                optimizer.step()
+                #최적화 단계
+                accelerator.clip_grad_norm_(model.parameters(), 1.0) #gradient bomb 방지
+                optimizer.step() #parameter 업데이트
                 lr_scheduler.step()
                 optimizer.zero_grad()
-            
+
+            #TensorBoard에 손실과 학습률을 logging하고 진행상황 출력            
             progress_bar.update(1)
             logs = {"loss": loss.detach().item(), "lr": lr_scheduler.get_last_lr()[0], "step": global_step}
             progress_bar.set_postfix(**logs)
             accelerator.log(logs, step=global_step)
             global_step += 1
 
-        # After each epoch you optionally sample some demo images with evaluate() and save the model
+        #현재 실행하는 프로세스가 mainprocess인지
+        #아래 작업을 단일 프로세스에서만 실행하도록 제한
+        #분산환경에서의 작업 logic이해가 필요한 | 하나의 메인프로세스 + worker들
+        #메인프로세스의 역할 : 전체 학습을 제어 (logging, 모델저장, 평가 등등)
+        #worker들의 역할 : 실제 데이터 처리와 계산
+
         if accelerator.is_main_process:
             pipeline = DDPMPipeline(unet=accelerator.unwrap_model(model), scheduler=noise_scheduler)
             #accelerator = Accelerator()
@@ -80,4 +102,4 @@ def train_loop(config, model, noise_scheduler, optimizer, train_dataloader, lr_s
                 evaluate(config, epoch, pipeline)
 
             if (epoch + 1) % config.save_model_epochs == 0 or epoch == config.num_epochs - 1:
-                pipeline.save_pretrained(config.output_dir) 
+                pipeline.save_pretrained(config.model_save_dir)
